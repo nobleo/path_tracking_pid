1b54e83656ca59f87901b213fa01fc5a576bb909
Lewie Donckers <lewie.donckers@nobleo.nl>
2022-02-15 13:24:02 +0000

    Forgot pragma once. And used ROS clang-format.

--- src/common.hpp
+++ src/common.hpp
@@ -1,12 +1,15 @@
+#pragma once
+
 #include <type_traits>
 
-namespace path_tracking_pid {
+namespace path_tracking_pid
+{
 
 // Converts an enumeration to its underlying type.
-template<typename enum_type>
+template <typename enum_type>
 constexpr std::underlying_type_t<enum_type> to_underlying(enum_type value) noexcept
 {
-    return static_cast<std::underlying_type_t<enum_type>>(value);
+  return static_cast<std::underlying_type_t<enum_type>>(value);
 }
 
-} // namespace path_tracking_pid
+}  // namespace path_tracking_pid
