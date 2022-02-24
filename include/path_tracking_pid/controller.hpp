27794c5225eb2e903394666e0dc66d451dcb9dbc
Lewie Donckers <lewie.donckers@nobleo.nl>
2022-02-24 07:34:39 +0000

    Reworked by adding documentation.

--- include/path_tracking_pid/controller.hpp
+++ include/path_tracking_pid/controller.hpp
@@ -200,6 +200,16 @@ private:
     double distance_to_w = 0;   // Distance in meter.
   };
 
+  /**
+   * @brief Closest point between a line segment and a point
+   * Calculate the closest point between the line segment bounded by PV and the point W.
+   * 
+   * @param[in] pose_p Start of the line segment
+   * @param[in] pose_v End of the line segment
+   * @param[in] pose_w The point
+   * @return The pose projection of the closest point with the distance (squared) to pose_p and
+   *         the distance to pose_w.
+   */
   DistToSegmentSquaredResult distToSegmentSquared(
     const tf2::Transform & pose_p, const tf2::Transform & pose_v,
     const tf2::Transform & pose_w) const;
