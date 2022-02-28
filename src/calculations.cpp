d2bcbbd1c036f409ba1c92c2188fea74ab13fb21
Lewie Donckers <lewie.donckers@nobleo.nl>
2022-02-28 14:39:55 +0000

    Keep clang-tidy happy.

--- src/calculations.cpp
+++ src/calculations.cpp
@@ -8,17 +8,17 @@
 namespace path_tracking_pid
 {
 
-std::vector<tf2::Transform> deltas_of_plan(const std::vector<tf2::Transform> & input)
+std::vector<tf2::Transform> deltas_of_plan(const std::vector<tf2::Transform> & plan)
 {
   auto result = std::vector<tf2::Transform>{};
 
-  if (input.size() < 2) {
+  if (plan.size() < 2) {
     return result;
   }
 
-  result.reserve(input.size() - 1);
+  result.reserve(plan.size() - 1);
   std::transform(
-    input.cbegin(), input.cend() - 1, input.cbegin() + 1, std::back_inserter(result),
+    plan.cbegin(), plan.cend() - 1, plan.cbegin() + 1, std::back_inserter(result),
     [](const tf2::Transform & a, const tf2::Transform & b) { return a.inverseTimes(b); });
 
   return result;
